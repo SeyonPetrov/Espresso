288d1b23450a50760ddf62511f947144650e6184
Tsoy <urta.1732@mail.ru>
2022-11-20 02:36:53 +0500

    привет мир!

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+
+
+class Coffee(QMainWindow):
+    def __init__(self):
+        super(Coffee, self).__init__()
+        uic.loadUi('main.ui', self)
+        self.pod = sqlite3.connect('coffee.sqlite3')
+        self.cu = self.pod.cursor()
+        data = self.cu.execute("select * from coffee").fetchall()
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(data):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                t = QTableWidgetItem(str(elem))
+                self.tableWidget.setItem(
+                    i, j, t)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    co = Coffee()
+    co.show()
+    sys.exit(app.exec())
